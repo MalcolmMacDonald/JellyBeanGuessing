a6d1f2084d78baa01845c77effdd88d4b94c8813
DESKTOP-1GU391B\Malcolm MacDonald <mac97@shaw.ca>
2019-03-05 11:09:22 -0800

    got bean fillling in

---
+++ Assets/Scripts/BeanSpawnerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeanSpawnerController : MonoBehaviour
+{
+
+    public GameObject beanPrefab;
+
+
+    public int beanCount;
+    public float timeBetweenSpawns;
+
+    public Color[] beanColors;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(SpawnBeans());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnBeans()
+    {
+        for (int i = 0; i < beanCount; i++)
+        {
+            GameObject spawnedBean = Instantiate(beanPrefab, transform.position, Random.rotation, transform);
+            spawnedBean.GetComponent<MeshRenderer>().materials[0].color = beanColors[Random.Range(0, beanColors.Length)];
+            yield return new WaitForSeconds(timeBetweenSpawns);
+        }
+
+    }
+}
--- Assets/Scripts/CameraPivotController.cs
+++ Assets/Scripts/CameraPivotController.cs
@@ -8,69 +8,40 @@ public class CameraPivotController : MonoBehaviour
 
 
     float horizontalAngle;
-    float horizontalVelocity;
-    float horizontalDestinationVelocity;
+
+    public int framesForCircle;
 
 
+    public delegate void OnComplete();
 
+    public OnComplete onComplete;
 
-    public float maxDragVelocity;
-    public float dragStrength;
-    public float velocityChangeRate;
-    public float decayRate;
-    public float maxVelocity;
-
-
-    Vector3 startingRotation;
-
-    Vector2 previousMousePos;
-
-    public AnimationCurve dragStrengthCurve;
-    public AnimationCurve decayRateCurve;
-    // Start is called before the first frame update
+    int frameCount;
+    Vector3 startRotation;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
-        startingRotation = transform.rotation.eulerAngles;
-        previousMousePos = (Vector2)Input.mousePosition;
+        startRotation = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButton(0))
+        frameCount++;
+
+        horizontalAngle = ((float)frameCount / framesForCircle) * 360f;
+
+        transform.rotation = Quaternion.Euler(startRotation.x, startRotation.y + horizontalAngle, startRotation.z);
+
+        if (frameCount >= framesForCircle)
         {
-            float dragVelocity = ((Vector2)Input.mousePosition - previousMousePos).x;
-            horizontalDestinationVelocity += dragStrengthCurve.Evaluate(Mathf.Clamp01(Mathf.Abs(dragVelocity) / maxDragVelocity)) * dragStrength * Mathf.Sign(dragVelocity);
+            frameCount = 0;
+            if (onComplete != null)
+            {
+                onComplete();
+            }
         }
-
-
-        float normalizedVelocity = Mathf.Clamp01(Mathf.Abs(horizontalDestinationVelocity) / maxVelocity);
-
-        if (horizontalDestinationVelocity > 0)
-        {
-            horizontalDestinationVelocity = Mathf.Clamp(horizontalDestinationVelocity - decayRate * Time.deltaTime * decayRateCurve.Evaluate(normalizedVelocity), 0, horizontalDestinationVelocity);
-        }
-        else
-        {
-            horizontalDestinationVelocity = Mathf.Clamp(horizontalDestinationVelocity + decayRate * Time.deltaTime * decayRateCurve.Evaluate(normalizedVelocity), horizontalDestinationVelocity, 0);
-        }
-
-
-        horizontalDestinationVelocity = Mathf.Sign(horizontalDestinationVelocity) * Mathf.Clamp(Mathf.Abs(horizontalDestinationVelocity), 0, maxVelocity);
-
-        horizontalVelocity = Mathf.MoveTowards(horizontalVelocity, horizontalDestinationVelocity, Time.deltaTime * velocityChangeRate);
-
-        horizontalAngle += horizontalVelocity * Time.deltaTime;
-
-        transform.rotation = Quaternion.Euler(startingRotation.x, horizontalAngle, startingRotation.z);
-        previousMousePos = (Vector2)Input.mousePosition;
-
-    }
-
-    void HorizontalMovement()
-    {
 
     }
 
